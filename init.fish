7cdb1157499e2a6fab8f59ebb2a9d173c009952d
Igor Kotrasinski <i.kotrasinsk@gmail.com>
2018-05-15 10:18:36 +0200

    Initialize globals at init
    Signed-off-by: Igor Kotrasinski <i.kotrasinsk@gmail.com>

--- init.fish
+++ init.fish
@@ -4,3 +4,11 @@
 # * $package       package name
 # * $path          package path
 # * $dependencies  package dependencies
+
+
+if not set -q _VISH_BOUND_KEYS
+	set -Ux _VISH_BOUND_KEYS (for i in (_vish_registers) ; echo "" ; end)
+end
+if not set -q _VISH_BOUND_DIRS
+	set -Ux _VISH_BOUND_DIRS (for i in (_vish_registers) ; echo "" ; end)
+end
