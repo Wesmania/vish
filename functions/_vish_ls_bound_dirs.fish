35cc281160df52a8566a838e1619c7b08072827e
Igor Kotrasinski <i.kotrasinsk@gmail.com>
2018-05-15 12:39:38 +0200

    Fix bad variable name
    Signed-off-by: Igor Kotrasinski <i.kotrasinsk@gmail.com>

--- functions/_vish_ls_bound_dirs.fish
+++ functions/_vish_ls_bound_dirs.fish
@@ -3,8 +3,8 @@ function _vish_ls_bound_dirs
 	set -l i 0
 	for key in (_vish_registers)
 		set -l i (math $i + 1)
-		if [ $_VISH_DIR_KEYS[$i] != "" ]
-			echo $key $_VISH_DIR_KEYS[$i]
+		if [ $_VISH_BOUND_DIRS[$i] != "" ]
+			echo $key $_VISH_BOUND_DIRS[$i]
 		end
 	end
 end
