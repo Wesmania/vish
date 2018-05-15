04eedd5fb69371332756bfa0188b502d881a2e31
Igor Kotrasinski <i.kotrasinsk@gmail.com>
2018-05-15 13:20:51 +0200

    Fix _vish_bind_sir set
    Signed-off-by: Igor Kotrasinski <i.kotrasinsk@gmail.com>

--- functions/_vish_bind_dir.fish
+++ functions/_vish_bind_dir.fish
@@ -2,6 +2,6 @@ function _vish_bind_dir -a key
 	if not set -q _VISH_BOUND_DIRS
 		return 1
 	end
-	set -l $idx (_vish_reg2idx $key)
+	set -l idx (_vish_reg2idx $key)
 	set -Ux _VISH_BOUND_DIRS[$idx] (pwd)
 end
