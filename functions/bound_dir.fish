ed9bb31b4262445fbfcf67f87b900d1649217d16
Igor Kotrasinski <i.kotrasinsk@gmail.com>
2018-05-15 09:54:00 +0200

    Add bound_dir script
    Signed-off-by: Igor Kotrasinski <i.kotrasinsk@gmail.com>

---
+++ functions/bound_dir.fish
@@ -0,0 +1,8 @@
+source ~/.config/fish/functions/register/tools.fish
+
+function bound_dir -a key
+	if not set -q _VISH_BOUND_DIRS
+		return 1
+	end
+	echo $_VISH_BOUND_DIRS[(_vish_key2idx $key)]
+end
