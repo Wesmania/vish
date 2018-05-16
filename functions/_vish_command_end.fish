042618db2c9f3cf844d48c50f48a3f0c7d487ced
Igor Kotrasinski <i.kotrasinsk@gmail.com>
2018-05-16 11:13:27 +0200

    Make some constants configurable
    Signed-off-by: Igor Kotrasinski <i.kotrasinsk@gmail.com>

--- functions/_vish_command_end.fish
+++ functions/_vish_command_end.fish
@@ -5,13 +5,19 @@ function _vish_command_end --on-event fish_postexec
 	if not _vish_prompt_set PING
 		return
 	end
-	for icmd in _VISH_INTERACTIVE_CMDS
+	for icmd in $VISH_INTERACTIVE_CMDS
 		if echo $argv[1] | grep $icmd > /dev/null ^ /dev/null
 			return
 		end
 	end
 
-	set -l alert_time 10
+	set -l alert_time ""
+	if set -q VISH_COMMAND_PING_MIN_TIME
+		set alert_time $VISH_COMMAND_PING_MIN_TIME
+	else
+		set alert_time 10
+	end
+
 	set interval (math (date +%s) - $_VISH_INTERACTIVE_COMMAND_START)
 	if [ "$interval" -gt "$alert_time" ]
 		if type -q _vish_ping
--- functions/_vish_prompt_path.fish
+++ functions/_vish_prompt_path.fish
@@ -1,9 +1,14 @@
 function _vish_prompt_path
 	set -l home_escaped (echo -n $HOME | sed 's/\//\\\\\//g')
 	set -l pwd (echo -n $PWD | sed "s/^$home_escaped/~/" | sed 's/ /%20/g')
+	set -l short_path ""
 	if _vish_prompt_set SHORT_PATH
+		if set -q VISH_PROMPT_SHORT_PATH_MAX_LENGTH
+			set short_path $VISH_PROMPT_SHORT_PATH_MAX_LENGTH
+		else
+			set short_path 50
+		end
 		set -l plen (echo $pwd | wc -c)
-		set -l short_path 50
 		if [ $plen -ge $short_path ]
 			set pwd "..."(echo $pwd | tail -c $short_path)
 		end
