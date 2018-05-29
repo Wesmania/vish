360d1f915cdf06026655325dc50d12ac50f97feb
Igor Kotrasinski <i.kotrasinsk@gmail.com>
2018-05-29 15:02:05 +0200

    fixes
    Signed-off-by: Igor Kotrasinski <i.kotrasinsk@gmail.com>

--- functions/_vish_unbind_dir.fish
+++ functions/_vish_unbind_dir.fish
@@ -2,5 +2,5 @@ function _vish_unbind_dir -a key
 	if not set -q _VISH_BOUND_DIRS
 		return 1
 	end
-	set -Ux _VISH_BOUND_DIRS[(_vish_key3idx $key)]
+	set -Ux _VISH_BOUND_DIRS[(_vish_reg2idx $key)] ''
 end
--- functions/bound_dir.fish
+++ functions/bound_dir.fish
@@ -1,5 +1,3 @@
-source ~/.config/fish/functions/register/tools.fish
-
 function bound_dir -a key
 	if not set -q _VISH_BOUND_DIRS
 		return 1
