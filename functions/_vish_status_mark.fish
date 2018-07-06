7cb059b3580188e9ca70ad5b70b246a142c1d553
Igor Kotrasinski <i.kotrasinsk@gmail.com>
2018-07-06 11:19:45 +0200

    Fancy colors for modes
    Signed-off-by: Igor Kotrasinski <i.kotrasinsk@gmail.com>

--- functions/_vish_status_mark.fish
+++ functions/_vish_status_mark.fish
@@ -1,10 +1,10 @@
 function _vish_status_mark -a bind_mode
 	switch $bind_mode
 		case vishcmd
-			echo 'V'
+			echo (set_color purple)'V'(set_color normal)
 		case vishregister
-			echo 'R'
+			echo (set_color green)'R'(set_color normal)
 		case vishregcmd
-			echo 'C'
+			echo (set_color yellow)"$_VISH_LAST_REGISTER"(set_color normal)
 	end
 end
