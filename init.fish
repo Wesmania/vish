59c88f5edafa9de74ab188150388f273f8ad52fb
Igor Kotrasinski <i.kotrasinsk@gmail.com>
2019-03-04 09:46:46 +0100

    Remove unused register init
    Signed-off-by: Igor Kotrasinski <i.kotrasinsk@gmail.com>

--- init.fish
+++ init.fish
@@ -5,10 +5,6 @@
 # * $path          package path
 # * $dependencies  package dependencies
 
-_vish_register_init KEYS
-_vish_register_init DIRS
-_vish_register_init PREFICES
-
 source $path/functions/_vish_mark_time.fish
 source $path/functions/_vish_command_end.fish
 source $path/functions/_vish_cmd_prefix.fish
