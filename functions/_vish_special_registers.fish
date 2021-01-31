e942c4c75247c3fa31baaee88155a0be279b5317
Igor Kotrasinski <i.kotrasinsk@gmail.com>
2021-01-31 21:12:42 +0100

    Fix string escape interpreting flags
    Signed-off-by: Igor Kotrasinski <i.kotrasinsk@gmail.com>

--- functions/_vish_special_registers.fish
+++ functions/_vish_special_registers.fish
@@ -4,7 +4,7 @@ function _vish_special_registers -a action -a key -a value
 		if not echo $kv[1] | grep -qF $key
 			continue
 		end
-		eval _vish_special_register_$kv[2] $action (string escape $key) (string escape $value)
+		eval _vish_special_register_$kv[2] $action (string escape -- $key) (string escape -- $value)
 		return
 	end
 	return 1
