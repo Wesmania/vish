1fc56f7c2ebb263db6517c47346b84a1ed388412
Igor Kotrasinski <i.kotrasinsk@gmail.com>
2022-01-05 14:57:18 +0100

    Delete snippets, replaced by tmux support
    Signed-off-by: Igor Kotrasinski <i.kotrasinsk@gmail.com>

--- snippets/tmux/_vish_pane_dir.fish
+++
@@ -1,9 +0,0 @@
-# Grabs pwd from i-th tmux pane.
-#
-# In fish_user_key_bindings:
-# _vish_make_num_param_register d "_vish_pane_dir" --doc "Grab path from i-th pane."
-# Call e.g. <L>1da to yank (1)st (d)irectory to register 0, then (a)ppend it.
-#
-function _vish_pane_dir -a num
-	tmux display-message -p -F "#{pane_current_path}" -t $num
-end
