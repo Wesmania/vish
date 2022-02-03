7be62878c94c9fe088b7106e6e315857381b7868
Igor Kotrasinski <i.kotrasinsk@gmail.com>
2022-02-03 11:04:57 +0100

    Fix tmux pane completion
    Signed-off-by: Igor Kotrasinski <i.kotrasinsk@gmail.com>

--- functions/_vish_tmux_complete.fish
+++ functions/_vish_tmux_complete.fish
@@ -21,10 +21,19 @@ function _vish_tmux_complete
 	# List panes, with active one first.
 	set -l panes (tmux list-panes -F '#{?pane_active,A,N} #{pane_index}' | sort | cut -f2 -d " ")
 
+	set -l first 1
 	for pane in $panes
 		for pattern in $start_pattern $mid_pattern
-			# Don't match the line the cursor is at, otherwise fish completion hints mess with tmux completion.
-			set -l endline (math (tmux display-message -p '#{cursor_y}') - 1)
+			# For current pane, don't match the line the cursor is at,
+			# otherwise fish completion hints mess with tmux completion.
+			set -l endline
+			if test $first -eq 1
+				set endline (math (tmux display-message -p '#{cursor_y}') - 1)
+			else
+				set endline (tmux display-message -p '#{pane_bottom}')
+			end
+			set first 0
+
 			set -l replacement (tmux capture-pane -t $pane -Jp -E $endline | grep -o $pattern | tail -n 1)
 			# Fix start pattern grabbing the first non-word character
 			set replacement (echo $replacement | grep -o $mid_pattern)
